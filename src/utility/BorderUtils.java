2f2312978eaf173ecf944ad39b8e8146fd4169c4
Furina <xuan040823@gmail.com>
2024-09-03 23:41:27 +0800

    fix attendance

--- src/utility/BorderUtils.java
+++ src/utility/BorderUtils.java
@@ -1,6 +1,5 @@
 package utility;
 
-import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
@@ -21,22 +20,5 @@ public class BorderUtils {
         return new CompoundBorder(lineBorder, emptyBorder);
     }
 
-    public static void main(String[] args) {
-        // Example usage
-        JFrame frame = new JFrame("Border Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
-
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.WHITE);
-        panel.setBorder(BorderUtils.createCustomBorder(Color.BLACK, 2, 5));
-
-        JLabel label = new JLabel("This is a label with a custom border.");
-        label.setBorder(BorderUtils.createCustomBorder(Color.RED, 1, 3));
-
-        panel.add(label);
-        frame.add(panel);
-        frame.setVisible(true);
-    }
 }
 
